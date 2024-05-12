dc4683181448c897bee6eb2671fab2584049933d
AlexD36 <alexduta36@gmail.com>
2024-05-13 02:01:25 +0300

    created login form

---
+++ UniCatalog/UniCatalog/Login_Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniCatalog
+{
+    public partial class buttonLogin : Form
+    {
+        public buttonLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
