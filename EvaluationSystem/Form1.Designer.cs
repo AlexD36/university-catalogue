86d2b12cbea8b828ec37e99441e4afa5fa999424
AlexD36 <alexduta36@gmail.com>
2024-06-06 16:03:13 +0300

    uploaded via GitDesktop

---
+++ EvaluationSystem/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace EvaluationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_find = new System.Windows.Forms.Button();
+            this.btn_reports = new System.Windows.Forms.Button();
+            this.btn_prerequisite = new System.Windows.Forms.Button();
+            this.btn_users = new System.Windows.Forms.Button();
+            this.btn_course = new System.Windows.Forms.Button();
+            this.btn_curriculumn = new System.Windows.Forms.Button();
+            this.btn_grades = new System.Windows.Forms.Button();
+            this.lblmenu = new System.Windows.Forms.Label();
+            this.btn_students = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.lblmenu);
+            this.panel1.Controls.Add(this.btn_login);
+            this.panel1.Controls.Add(this.btn_find);
+            this.panel1.Controls.Add(this.btn_reports);
+            this.panel1.Controls.Add(this.btn_prerequisite);
+            this.panel1.Controls.Add(this.btn_users);
+            this.panel1.Controls.Add(this.btn_course);
+            this.panel1.Controls.Add(this.btn_curriculumn);
+            this.panel1.Controls.Add(this.btn_grades);
+            this.panel1.Controls.Add(this.btn_students);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(162, 498);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.Black;
+            this.btn_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_login.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_login.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_login.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(0, 360);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(162, 45);
+            this.btn_login.TabIndex = 10;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            this.btn_login.MouseLeave += new System.EventHandler(this.btn_login_MouseLeave);
+            this.btn_login.MouseHover += new System.EventHandler(this.btn_login_MouseHover);
+            // 
+            // btn_find
+            // 
+            this.btn_find.BackColor = System.Drawing.Color.Black;
+            this.btn_find.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_find.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_find.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_find.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_find.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_find.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_find.ForeColor = System.Drawing.Color.White;
+            this.btn_find.Location = new System.Drawing.Point(0, 315);
+            this.btn_find.Name = "btn_find";
+            this.btn_find.Size = new System.Drawing.Size(162, 45);
+            this.btn_find.TabIndex = 9;
+            this.btn_find.Text = "Find Students";
+            this.btn_find.UseVisualStyleBackColor = false;
+            this.btn_find.Click += new System.EventHandler(this.btn_find_Click);
+            this.btn_find.MouseLeave += new System.EventHandler(this.btn_find_MouseLeave);
+            this.btn_find.MouseHover += new System.EventHandler(this.btn_find_MouseHover);
+            // 
+            // btn_reports
+            // 
+            this.btn_reports.BackColor = System.Drawing.Color.Black;
+            this.btn_reports.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_reports.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_reports.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_reports.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_reports.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_reports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_reports.ForeColor = System.Drawing.Color.White;
+            this.btn_reports.Location = new System.Drawing.Point(0, 270);
+            this.btn_reports.Name = "btn_reports";
+            this.btn_reports.Size = new System.Drawing.Size(162, 45);
+            this.btn_reports.TabIndex = 8;
+            this.btn_reports.Text = "Generate Reports";
+            this.btn_reports.UseVisualStyleBackColor = false;
+            this.btn_reports.Click += new System.EventHandler(this.btn_reports_Click);
+            this.btn_reports.MouseLeave += new System.EventHandler(this.btn_reports_MouseLeave);
+            this.btn_reports.MouseHover += new System.EventHandler(this.btn_reports_MouseHover);
+            // 
+            // btn_prerequisite
+            // 
+            this.btn_prerequisite.BackColor = System.Drawing.Color.Black;
+            this.btn_prerequisite.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_prerequisite.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_prerequisite.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_prerequisite.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_prerequisite.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_prerequisite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_prerequisite.ForeColor = System.Drawing.Color.White;
+            this.btn_prerequisite.Location = new System.Drawing.Point(0, 225);
+            this.btn_prerequisite.Name = "btn_prerequisite";
+            this.btn_prerequisite.Size = new System.Drawing.Size(162, 45);
+            this.btn_prerequisite.TabIndex = 7;
+            this.btn_prerequisite.Text = "Manage Pre-Requisites";
+            this.btn_prerequisite.UseVisualStyleBackColor = false;
+            this.btn_prerequisite.Click += new System.EventHandler(this.btn_prerequisite_Click);
+            this.btn_prerequisite.MouseLeave += new System.EventHandler(this.btn_prerequisite_MouseLeave);
+            this.btn_prerequisite.MouseHover += new System.EventHandler(this.btn_prerequisite_MouseHover);
+            // 
+            // btn_users
+            // 
+            this.btn_users.BackColor = System.Drawing.Color.Black;
+            this.btn_users.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_users.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_users.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_users.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_users.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_users.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_users.ForeColor = System.Drawing.Color.White;
+            this.btn_users.Location = new System.Drawing.Point(0, 180);
+            this.btn_users.Name = "btn_users";
+            this.btn_users.Size = new System.Drawing.Size(162, 45);
+            this.btn_users.TabIndex = 6;
+            this.btn_users.Text = "Manage Users";
+            this.btn_users.UseVisualStyleBackColor = false;
+            this.btn_users.Click += new System.EventHandler(this.btn_users_Click);
+            this.btn_users.MouseLeave += new System.EventHandler(this.btn_users_MouseLeave);
+            this.btn_users.MouseHover += new System.EventHandler(this.btn_users_MouseHover);
+            // 
+            // btn_course
+            // 
+            this.btn_course.BackColor = System.Drawing.Color.Black;
+            this.btn_course.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_course.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_course.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_course.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_course.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_course.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_course.ForeColor = System.Drawing.Color.White;
+            this.btn_course.Location = new System.Drawing.Point(0, 135);
+            this.btn_course.Name = "btn_course";
+            this.btn_course.Size = new System.Drawing.Size(162, 45);
+            this.btn_course.TabIndex = 5;
+            this.btn_course.Text = "Manage Courses";
+            this.btn_course.UseVisualStyleBackColor = false;
+            this.btn_course.Click += new System.EventHandler(this.btn_course_Click);
+            this.btn_course.MouseLeave += new System.EventHandler(this.btn_course_MouseLeave);
+            this.btn_course.MouseHover += new System.EventHandler(this.btn_course_MouseHover);
+            // 
+            // btn_curriculumn
+            // 
+            this.btn_curriculumn.BackColor = System.Drawing.Color.Black;
+            this.btn_curriculumn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_curriculumn.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_curriculumn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_curriculumn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_curriculumn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_curriculumn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_curriculumn.ForeColor = System.Drawing.Color.White;
+            this.btn_curriculumn.Location = new System.Drawing.Point(0, 90);
+            this.btn_curriculumn.Name = "btn_curriculumn";
+            this.btn_curriculumn.Size = new System.Drawing.Size(162, 45);
+            this.btn_curriculumn.TabIndex = 4;
+            this.btn_curriculumn.Text = "Manage Curriculumn";
+            this.btn_curriculumn.UseVisualStyleBackColor = false;
+            this.btn_curriculumn.Click += new System.EventHandler(this.btn_curriculumn_Click);
+            this.btn_curriculumn.MouseLeave += new System.EventHandler(this.btn_curriculumn_MouseLeave);
+            this.btn_curriculumn.MouseHover += new System.EventHandler(this.btn_curriculumn_MouseHover);
+            // 
+            // btn_grades
+            // 
+            this.btn_grades.BackColor = System.Drawing.Color.Black;
+            this.btn_grades.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_grades.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_grades.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_grades.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_grades.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_grades.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_grades.ForeColor = System.Drawing.Color.White;
+            this.btn_grades.Location = new System.Drawing.Point(0, 45);
+            this.btn_grades.Name = "btn_grades";
+            this.btn_grades.Size = new System.Drawing.Size(162, 45);
+            this.btn_grades.TabIndex = 3;
+            this.btn_grades.Text = "Manage Grades";
+            this.btn_grades.UseVisualStyleBackColor = false;
+            this.btn_grades.Click += new System.EventHandler(this.btn_grades_Click);
+            this.btn_grades.MouseLeave += new System.EventHandler(this.btn_grades_MouseLeave);
+            this.btn_grades.MouseHover += new System.EventHandler(this.btn_grades_MouseHover);
+            // 
+            // lblmenu
+            // 
+            this.lblmenu.BackColor = System.Drawing.Color.Red;
+            this.lblmenu.ForeColor = System.Drawing.Color.Red;
+            this.lblmenu.Location = new System.Drawing.Point(1, 0);
+            this.lblmenu.Name = "lblmenu";
+            this.lblmenu.Size = new System.Drawing.Size(10, 45);
+            this.lblmenu.TabIndex = 2;
+            // 
+            // btn_students
+            // 
+            this.btn_students.BackColor = System.Drawing.Color.Black;
+            this.btn_students.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_students.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn_students.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.btn_students.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btn_students.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btn_students.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_students.ForeColor = System.Drawing.Color.White;
+            this.btn_students.Location = new System.Drawing.Point(0, 0);
+            this.btn_students.Name = "btn_students";
+            this.btn_students.Size = new System.Drawing.Size(162, 45);
+            this.btn_students.TabIndex = 2;
+            this.btn_students.Text = "Manage Students";
+            this.btn_students.UseVisualStyleBackColor = false;
+            this.btn_students.Click += new System.EventHandler(this.btn_students_Click);
+            this.btn_students.MouseLeave += new System.EventHandler(this.btn_students_MouseLeave);
+            this.btn_students.MouseHover += new System.EventHandler(this.btn_students_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(162, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(752, 90);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Copyrights @janobe Source Code ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 498);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Management System | Main Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_find;
+        private System.Windows.Forms.Button btn_reports;
+        private System.Windows.Forms.Button btn_prerequisite;
+        private System.Windows.Forms.Button btn_users;
+        private System.Windows.Forms.Button btn_course;
+        private System.Windows.Forms.Button btn_curriculumn;
+        private System.Windows.Forms.Button btn_grades;
+        private System.Windows.Forms.Label lblmenu;
+        private System.Windows.Forms.Button btn_students;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EvaluationSystem/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void enabled_menu()
+        {
+            btn_login .Text = "Logout";
+            btn_students .Enabled = true;
+            btn_course .Enabled = true;
+            btn_grades .Enabled = true;
+            btn_curriculumn .Enabled = true;
+            btn_users .Enabled = true;
+            btn_reports .Enabled = true;
+            btn_find .Enabled = true;
+            btn_prerequisite.Enabled = true;
+        }
+
+        public void disabled_menu()
+        {
+            btn_login.Text = "Login";
+            btn_students.Enabled = false;
+            btn_course.Enabled = false;
+            btn_grades.Enabled = false;
+            btn_curriculumn.Enabled = false;
+            btn_users.Enabled = false;
+            btn_reports.Enabled = false;
+            btn_find.Enabled = false;
+            btn_prerequisite.Enabled = false ;
+        }
+
+
+        private void closefrm()
+        {
+            foreach(Form frm in this.MdiChildren)
+            {
+                frm.Close();
+
+            }
+        }
+        private void showfrm(Form frm,Button btn)
+        {
+            lblmenu.Left = btn.Left;
+            lblmenu.Height = btn.Height;
+            lblmenu.Location = btn.Location;
+
+            this.IsMdiContainer = true;
+            frm.MdiParent = this;
+            frm.Show();
+
+
+        }
+
+        private void hover(Button btn,EventArgs e)
+        {
+            btn.BackColor = Color.Red;
+        }
+
+        private void leave(Button btn, EventArgs e)
+        {
+            btn.BackColor = Color.Transparent ;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            disabled_menu();
+        }
+ 
+
+        private void btn_students_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmStudent(), btn_students);
+              
+        }
+
+        private void btn_grades_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmGrades (), btn_grades);
+        }
+
+        private void btn_curriculumn_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmSubjects(),btn_curriculumn);
+        }
+
+        private void btn_course_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmCourse (),btn_course);
+        }
+
+        private void btn_users_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmUsers (),btn_users);
+        }
+
+        private void btn_prerequisite_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmPreRequisite (),btn_prerequisite);
+        }
+
+        private void btn_reports_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmReports (),btn_reports);
+        }
+
+        private void btn_find_Click(object sender, EventArgs e)
+        {
+            closefrm();
+            showfrm(new frmFinstudents (),btn_find);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (btn_login.Text == "Login")
+            {
+                closefrm();
+                showfrm(new frmLogin(this),btn_login);
+            }
+            else
+            {
+                disabled_menu();
+            }
+        }
+
+        private void btn_students_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_students, e);
+        }
+
+        private void btn_students_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_students, e);
+        }
+
+        private void btn_grades_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_grades, e);
+        }
+
+        private void btn_grades_MouseLeave(object sender, EventArgs e)
+        {
+            leave (btn_grades , e);
+        }
+
+        private void btn_curriculumn_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_curriculumn, e);
+        }
+
+        private void btn_curriculumn_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_curriculumn, e);
+        }
+
+        private void btn_course_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_course, e);
+        }
+ 
+
+        private void btn_users_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_users, e);
+        }
+
+        private void btn_users_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_users, e);
+        }
+
+        private void btn_prerequisite_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_prerequisite, e);
+        }
+
+        private void btn_prerequisite_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_prerequisite, e);
+        }
+
+        private void btn_reports_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_reports, e);
+        }
+
+        private void btn_reports_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_reports, e);
+        }
+
+        private void btn_find_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_find, e);
+        }
+
+        private void btn_find_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_find, e);
+        }
+
+        private void btn_login_MouseHover(object sender, EventArgs e)
+        {
+            hover(btn_login, e);
+        }
+
+        private void btn_login_MouseLeave(object sender, EventArgs e)
+        {
+            leave(btn_login, e);
+
+        }
+
+        private void btn_course_MouseHover(object sender, EventArgs e)
+        {
+            hover (btn_course, e);
+        }
+    } 
+}
